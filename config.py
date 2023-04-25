6b0146a671fbe5886341f28a9196d3509193a526
kalebss <kaleb.saldana@getsales.team>
2023-04-25 09:01:21 -0500

    first pass api

---
+++ config.py
@@ -0,0 +1,6 @@
+
+
+# spotfy_api
+spotify_redirect = "https://asathewonderer.com/spotify-uri"
+spotify_client_id = "6faeb6f6a44a4c609c5c3cac1c342e1a"
+spotify_secret_id = "3d5cbba5ff464350843216bebf9bb30b"
