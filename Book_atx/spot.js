b3ab39e43fb64e7d6ea7b82f1d87ca2ab5a77de7
kalebss <kaleb.saldana@getsales.team>
2023-07-06 20:23:45 -0500

    commiting utilities and classes

---
+++ Book_atx/spot.js
@@ -0,0 +1,41 @@
+
+
+var request = require('request'); // "Request" library
+
+var client_id = config.client_id
+var client_secret = config.client_secret
+var playlist_id = "5qiKyBjtLFJyPRDo8RfIiH"
+
+// your application requests authorization
+var authOptions = {
+  url: 'https://accounts.spotify.com/api/token',
+  headers: {
+    'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+  },
+  form: {
+    grant_type: 'client_credentials'
+  },
+  json: true
+};
+
+request.post(authOptions, function(error, response, body) {
+  if (!error && response.statusCode === 200) {
+
+    // use the access token to access the Spotify Web API
+    var token = body.access_token;
+    var options = {
+      url: 'https://api.spotify.com/v1/playlists/5qiKyBjtLFJyPRDo8RfIiH',
+      headers: {
+        'Authorization': 'Bearer ' + token
+      },
+      json: true
+    };
+    request.get(options, function(error, response, body) {
+      console.log(body);
+    });
+  }
+});
+
+curl -X POST "https://accounts.spotify.com/api/token" \
+     -H "Content-Type: application/x-www-form-urlencoded" \
+     -d "grant_type=client_credentials&client_id=your-client-id&client_secret=your-client-secret"
